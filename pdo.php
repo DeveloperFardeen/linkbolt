f760191246761e97781364b063f971f5e3da176b
Fardeen The Developer <fardeenthedeveloper@gmail.com>
2026-02-14 09:08:02 +0530

    db conn

--- pdo.php
+++ pdo.php
@@ -1,8 +1,8 @@
 <?php
-$host = '193.203.184.3';
-$db   = 'u214833083_linkBolt';
-$user = 'u214833083_fardeen3';
-$pass = 'huu_7G776y767iHY76THy65gh8H886Hj';
+$host = 'localhost';
+$db   = 'link_bolt';
+$user = 'root';
+$pass = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
